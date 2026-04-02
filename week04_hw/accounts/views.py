36f6992e7198ce42ddca957c999dc258b5e405e6
24yp01 <24scarlett10@sookmyung.ac.kr>
2026-04-02 09:35:33 +0900

    Feat: 회원가입 기능 추가

--- week04_hw/accounts/views.py
+++ week04_hw/accounts/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
+def Signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'signup.html', {'form': form})
 
 # Create your views here.
