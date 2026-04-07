648dff5a05988d28666d464d01e55a5a5870bd86
24yp01 <24scarlett10@sookmyung.ac.kr>
2026-04-07 20:35:30 +0900

    [Feat] 나만의 운세 뽑기 프로그램 구현

---
+++ FEweek01/practice.js
@@ -0,0 +1,46 @@
+let first = "youngseo"
+let last = "park"
+console.log(`My name is ${first} ${last}.
+    I'm ${20+2} years old.`)
+
+
+let likelion;
+likelion = "null";
+console.log(likelion);
+
+
+const daysOfWeek = ['1', '2', '3', '4', '5', '6'];
+console.log(daysOfWeek);
+daysOfWeek.push('7');
+console.log(daysOfWeek);
+const firstDay = daysOfWeek[0];
+console.log(firstDay);
+
+
+const myInfo = {
+    name: 'youngseo',
+    age: 22,
+    major: 'artificial intelligence',
+    tired: true,
+};
+console.log(myInfo);
+myInfo.tired = false;
+myInfo.hungry = true;
+console.log(myInfo);
+
+
+let x = 10;
+let result = x % 2 ? '홀수' : '짝수';
+console.log(result);
+
+
+let score = 85;
+if (score >= 90) {
+    console.log('A');
+} else if (score >= 80) {
+    console.log('B');
+} else {
+    console.log('C');
+}
+
+
