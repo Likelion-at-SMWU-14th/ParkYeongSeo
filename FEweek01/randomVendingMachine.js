220317b0870734eb66a1307fe061e690c0a3666f
24yp01 <24scarlett10@sookmyung.ac.kr>
2026-04-08 22:40:36 +0900

    [Chore] 가격 안내 추가

--- FEweek01/randomVendingMachine.js
+++ FEweek01/randomVendingMachine.js
@@ -8,3 +8,28 @@ const drinks = {
     딸기에이드: 3000,
     레몬에이드: 3000,
 };
+
+function buyDrink() {
+    let wallet = 5000;
+    let selected = 0;
+
+    const keys = Object.keys(drinks);
+    const values = Object.values(drinks);
+
+    for (var i = 1; i <= 3; i++) {
+        selected = Math.floor(Math.random() * values.length);
+
+        if (wallet >= values[selected]){
+            wallet -= values[selected];
+            console.log(keys[selected]);
+            console.log( `음료가 나왔어요! (가격: ${values[selected]})
+                지갑에 남은 돈: ${wallet}원`);
+        }
+
+        else{
+            console.log(`돈이 부족해요! 음료를 살 수 없어요!`);
+        }
+    };
+};
+
+buyDrink();
