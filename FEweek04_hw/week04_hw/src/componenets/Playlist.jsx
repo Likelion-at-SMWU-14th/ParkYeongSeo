67b96d4ad0916489b695c993e0195954bc9be1fb
24yp01 <24scarlett10@sookmyung.ac.kr>
2026-05-22 20:56:49 +0900

    [Chore] 링크 연결

--- FEweek04_hw/week04_hw/src/componenets/Playlist.jsx
+++ FEweek04_hw/week04_hw/src/componenets/Playlist.jsx
@@ -19,12 +19,23 @@ const BorderText = styled.p`
   white-space: nowrap;
 `
 
-const MusicWrapper = styled.div`
+const MusicWrapper = styled.a`
   display: flex;
   justify-content: space-between;
   align-items: center;
   width: 100%;
   padding: 12px 0;
+  text-decoration: none;
+
+  cursor: pointer;
+
+  &:hover {
+    background-color: #0c0c0c;
+  }
+
+  &:hover p {
+    color: #fff;
+  }
 `
 
 const MusicText = styled.p`
@@ -32,7 +43,6 @@ const MusicText = styled.p`
   font-size: 16px;
   font-weight: 700;
   color: #0c0c0c;
-
   margin: 0;
 `
 
@@ -41,7 +51,6 @@ const SingerText = styled.p`
   font-size: 16px;
   font-weight: 700;
   color: #0c0c0c;
-
   margin: 0;
 `
 
@@ -49,37 +58,37 @@ const Playlist = () => {
   return (
     <PlaylistWrapper>
       <BorderText>-----------------------------------------------</BorderText>
-      <MusicWrapper>
+      <MusicWrapper href='https://youtu.be/6Ycn9qZK09I?si=bqDQEW3VBaujGrMd' target='_blank'>
         <MusicText>Heavy Serenade</MusicText>
         <SingerText>NMIXX</SingerText>
       </MusicWrapper>
       <BorderText>-----------------------------------------------</BorderText>
-      <MusicWrapper>
+      <MusicWrapper href='https://youtu.be/odia4mSCOTc?si=5qNAOekIcIPcu5PH' target='_blank'>
         <MusicText>All the Possibilities</MusicText>
         <SingerText>TWS</SingerText>
       </MusicWrapper>
       <BorderText>-----------------------------------------------</BorderText>
-      <MusicWrapper>
+      <MusicWrapper href='https://youtu.be/oUDNUv7D3n8?si=kYSAKwBXEZ0gZaY1' target='_blank'>
         <MusicText>Toki Yo Tomare</MusicText>
         <SingerText>ILLIT</SingerText>
       </MusicWrapper>
       <BorderText>-----------------------------------------------</BorderText>
-      <MusicWrapper>
+      <MusicWrapper href='https://youtu.be/i5S7DBQnqP0?si=PEJqwkRATDOHi5tp' target='_blank'>
         <MusicText>Eye-Poppin'</MusicText>
         <SingerText>KickFlip</SingerText>
       </MusicWrapper>
       <BorderText>-----------------------------------------------</BorderText>
-      <MusicWrapper>
+      <MusicWrapper href='https://youtu.be/ctfS0Rcq-gs?si=6hBA13T4o0tW92Nn' target='_blank'>
         <MusicText>BEEP</MusicText>
         <SingerText>izna</SingerText>
       </MusicWrapper>
       <BorderText>-----------------------------------------------</BorderText>
-      <MusicWrapper>
+      <MusicWrapper href='https://youtu.be/Z-hpRjpFmmU?si=ZdNPkkyYXCMhdOsv' target='_blank'>
         <MusicText>Like It Like It</MusicText>
         <SingerText>TWICE</SingerText>
       </MusicWrapper>
       <BorderText>-----------------------------------------------</BorderText>
-      <MusicWrapper>
+      <MusicWrapper href='https://youtu.be/Oz7v7TP_-3k?si=y6UAjMbpZSsHYn0E' target='_blank'>
         <MusicText>I Like It</MusicText>
         <SingerText>Stray Kids</SingerText>
       </MusicWrapper>
