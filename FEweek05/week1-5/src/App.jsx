2ec78cebbe74829c292a5b576861c1cbf63af622
24yp01 <24scarlett10@sookmyung.ac.kr>
2026-06-23 20:16:28 +0900

    feat: 계산기 기본 구조 작성

--- FEweek05/week1-5/src/App.jsx
+++ FEweek05/week1-5/src/App.jsx
@@ -1,7 +1,37 @@
-import "./App.css";
+import * as S from "./styles/Calculator.Styled";
 
-function App(){
-  return <></>;
+function App() {
+  return (
+    <S.Page>
+      <S.CalculatorPanel>
+        <S.Display>0</S.Display>
+
+        <S.ButtonGrid>
+          <S.TemporaryButton>AC</S.TemporaryButton>
+          <S.TemporaryButton>÷</S.TemporaryButton>
+
+          <S.TemporaryButton>7</S.TemporaryButton>
+          <S.TemporaryButton>8</S.TemporaryButton>
+          <S.TemporaryButton>9</S.TemporaryButton>
+          <S.TemporaryButton>×</S.TemporaryButton>
+
+          <S.TemporaryButton>4</S.TemporaryButton>
+          <S.TemporaryButton>5</S.TemporaryButton>
+          <S.TemporaryButton>6</S.TemporaryButton>
+          <S.TemporaryButton>-</S.TemporaryButton>
+
+          <S.TemporaryButton>1</S.TemporaryButton>
+          <S.TemporaryButton>2</S.TemporaryButton>
+          <S.TemporaryButton>3</S.TemporaryButton>
+          <S.TemporaryButton>+</S.TemporaryButton>
+
+          <S.TemporaryButton>0</S.TemporaryButton>
+          <S.TemporaryButton>.</S.TemporaryButton>
+          <S.TemporaryButton>=</S.TemporaryButton>
+        </S.ButtonGrid>
+      </S.CalculatorPanel>
+    </S.Page>
+  );
 }
 
 export default App;
---
+++ FEweek05/week1-5/src/styles/Calculator.Styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Page = styled.main`
+min-height: 100vh;
+`;
+
+export const CalculatorPanel = styled.section`
+    width: 100vh;
+`;
+
+export const Display = styled.div`
+    min-height: 0.5rem;
+    padding: 1.5rem;
+`;
+
+export const ButtonGrid = styled.div`
+    width: 100%;
+`;
+
+export const TemporaryButton = styled.div`
+    min-height: 5rem;
+`;
