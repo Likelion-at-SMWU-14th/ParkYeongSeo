9d8d33e699ae229f6fe837cd5324a6042c84fa46
24yp01 <24scarlett10@sookmyung.ac.kr>
2026-06-30 16:29:12 +0900

    feat: useEffect 의존성 배열 실습

--- FEweek06/week06/src/App.jsx
+++ FEweek06/week06/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css"
 import Info from "./components/Info";
+import Search from "./components/DebouncedSearch"
 
 function App() {
   return (
     <div>
       <Info />
+      <Search />
     </div>
   );
 }
--- FEweek06/week06/src/components/Info.jsx
+++ FEweek06/week06/src/components/Info.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react"
+import { useState, useEffect} from "react"
 
 
 function Info() {
     const [name, setName]= useState("")
     const [nickname, setNickname] = useState("")
-    
+
+    useEffect(() => {
+        console.log("nickname이 변경될 때마다 렌더링");
+        console.log({ nickname });
+    }, [nickname]);
+
     return (
         <div>
             <input
