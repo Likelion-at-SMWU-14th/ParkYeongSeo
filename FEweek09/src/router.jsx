e4f0910ebebbd0364b10c6ecd5fcae01b051d8ff
24yp01 <24scarlett10@sookmyung.ac.kr>
2026-07-21 16:37:03 +0900

    feat: HttpOnly 쿠키 기반 JWT 로그인 구현

--- FEweek09/src/router.jsx
+++ FEweek09/src/router.jsx
@@ -1,10 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import CommentPage from "./pages/CommentPage";
+import DetailPage from "./pages/DetailPage";
+import WritePage from "./pages/WritePage";
+import LoginPage from "./pages/LoginPage";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <CommentPage />,
+            },
+            {
+                path: "detail/:id",
+                element: <DetailPage />
+            },
+            {
+                path: "write",
+                element: <WritePage />
+            },
+            {
+                path: "/login",
+                element: <LoginPage />
+            },
+        ],
     },
 ]);
 
