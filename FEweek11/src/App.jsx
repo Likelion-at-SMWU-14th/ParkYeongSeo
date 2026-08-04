9757f549bc58318752d7f97ca8b10f5997259c87
24yp01 <24scarlett10@sookmyung.ac.kr>
2026-08-04 16:00:45 +0900

    feat: ThemeProvider 적용

--- FEweek11/src/App.jsx
+++ FEweek11/src/App.jsx
@@ -2,13 +2,16 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
 import TitleContext from "./contexts/TitleContext";
+import { ThemeContextProvider } from "./contexts/ThemeContext";
 
 function App() {
   return (
-    <TitleContext.Provider value={"youngseo의 영화관"}>
-      <Header />
-      <Outlet />
-    </TitleContext.Provider>
+    <ThemeContextProvider>
+      <TitleContext.Provider value={"youngseo의 영화관"}>
+        <Header />
+        <Outlet />
+      </TitleContext.Provider>
+    </ThemeContextProvider>
   );
 }
 
