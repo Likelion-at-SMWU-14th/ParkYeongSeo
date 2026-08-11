3b86c28c7b638a6721a15b3c1780534b9b026a84
24yp01 <24scarlett10@sookmyung.ac.kr>
2026-08-11 16:10:45 +0900

    feat: DevTools middleware 적용

--- FEweek12/src/store/store.js
+++ FEweek12/src/store/store.js
@@ -1,25 +1,38 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
-const useTodoStore = create((set) => ({
+const useTodoStore = create(
+    devtools((set) => ({
     todos: [],
     addTodo: (rawTitle) => {
         const title = rawTitle.trim();
         if (!title) return;
         set((state) => ({
             todos: [...state.todos, { id: Date.now(), title, completed: false }],
-        }));
+        }),
+        undefined,
+        "todo/addTodo",
+        );
     },
     removeTodo: (id) =>
-        set((state) => ({
+        set(
+        (state) => ({
             todos: state.todos.filter((todo) => todo.id !== id),
-        })),
+        }),
+        undefined,
+        "todo/removeTodo",
+    ),
     toggleTodo: (id) =>
-        set((state) => ({
-            todos: state.todos.map((todo) =>
-                todo.id === id ? { ...todo, completed: !todo.completed } : todo,
-            ),
-        })),
-}));
+        set(
+            (state) => ({
+                todos: state.todos.map((todo) =>
+                    todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+                ),
+            }),
+            undefined,
+            "todo/toggleTodo",
+        ),
+    })),
+);
 
 export default useTodoStore;
