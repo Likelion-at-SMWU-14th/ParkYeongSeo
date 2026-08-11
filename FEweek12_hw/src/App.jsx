9d33f504e565344ed8c5b17c316fa1e0b131d82b
박영서 <24scarlett10@sookmyung.ac.kr>
2026-08-12 00:55:40 +0900

    feat: 레이아웃 배치

--- FEweek12_hw/src/App.jsx
+++ FEweek12_hw/src/App.jsx
@@ -5,6 +5,7 @@ import PlaylistPanel from './components/PlaylistPanel';
 export default function App() {
   return (
     <Notebook>
+      <Spine src="/spine.png" alt="" />
       <SearchPanel />
       <PlaylistPanel />
     </Notebook>
@@ -18,7 +19,20 @@ const Notebook = styled.div`
   background: #bfe0f5;
   border-radius: 28px;
   padding: 18px;
+  position: relative;
   display: grid;
   grid-template-columns: 1fr 1fr;
-  gap: 16px;
+  gap: 0;
+`;
+
+const Spine = styled.img`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  height: 100%;
+  width: 40px;
+  z-index: 2;
+  pointer-events: none;
 `;
