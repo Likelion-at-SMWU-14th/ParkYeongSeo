3e22cec105e7a0d4eda462ad1f409c6964fd0d6b
박영서 <24scarlett10@sookmyung.ac.kr>
2026-08-12 00:47:47 +0900

    feat: app.jsx 구성

--- FEweek12_hw/src/App.jsx
+++ FEweek12_hw/src/App.jsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import styled from 'styled-components';
+import SearchPanel from './components/SearchPanel';
+import PlaylistPanel from './components/PlaylistPanel';
 
-function App() {
-  return 
-  <></>
+export default function App() {
+  return (
+    <Notebook>
+      <SearchPanel />
+      <PlaylistPanel />
+    </Notebook>
+  );
 }
 
-export default App;
+
+const Notebook = styled.div`
+  width: min(1200px, 100%);
+  margin: 40px auto;
+  background: #bfe0f5;
+  border-radius: 28px;
+  padding: 18px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 16px;
+`;
